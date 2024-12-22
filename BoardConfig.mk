d4705f44c5f381c8e03adec355804b77713198e7
Alexander Lybenko <alexander.ly@askimo.ru>
2024-12-22 23:49:40 +0600

    flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,8 @@
 
 DEVICE_PATH := device/digma/TS1088ML
 
+TARGET_GLOBAL_CFLAGS += -Wno-error=implicit-fallthrough -Wno-error=stringop-truncation
+TARGET_GLOBAL_CPPFLAGS += -Wno-error=implicit-fallthrough -Wno-error=stringop-truncation
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
